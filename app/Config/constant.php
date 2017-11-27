9fc9c163691c7e46a4ca24ddacead42b5f1d3a55
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-11-27 17:47:54 +0700

    update

---
+++ app/Config/constant.php
@@ -0,0 +1,24 @@
+<?php
+
+    //get IP address from user
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = '';
+
+    define('CLIENT_IP_ADRS', $ipaddress);
+
+    define('INSERT', 0);
+    define('UPDATE', 1);
+    define('DELETE', 9);
+
